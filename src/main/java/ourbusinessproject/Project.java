9e00421612807e90e0d87669d48d866558b945b4
Henry Monteil <henry.monteil@gmail.com>
2020-01-29 14:29:46 +0100

    fix 1.2.4 classe Project

---
+++ src/main/java/ourbusinessproject/Project.java
@@ -0,0 +1,23 @@
+package ourbusinessproject;
+
+import com.sun.istack.NotNull;
+
+import javax.validation.constraints.NotBlank;
+
+public class Project {
+
+    @NotNull
+    @NotBlank
+    private String title;
+
+    private String description;
+
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
