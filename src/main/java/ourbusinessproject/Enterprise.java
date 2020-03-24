a033f3fb9d92ff5dda3c924e04b8230e805c63d0
henry <henry.monteil@gmail.com>
2020-03-24 11:32:33 +0100

    fix #2.1.9 service improvement

--- src/main/java/ourbusinessproject/Enterprise.java
+++ src/main/java/ourbusinessproject/Enterprise.java
@@ -1,73 +1,60 @@
 package ourbusinessproject;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import org.hibernate.validator.constraints.Length;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
-
+import java.util.ArrayList;
+import java.util.List;
 @Entity
 public class Enterprise {
-
     @Id
-    @GeneratedValue
-    private Long id;
-
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
     @NotEmpty
     private String name;
-    @Size(min = 10)
+    @NotEmpty @Length(min = 10, message = "The field must be at least 10 characters")
     private String description;
     @NotEmpty
     private String contactName;
     @NotEmpty @Email
     private String contactEmail;
+    @OneToMany(cascade=CascadeType.ALL)
+    private List<Project> projects;
 
-    @OneToMany
-    private Project projects;
-
+    public Enterprise(){
+        this.projects = new ArrayList<>();
+    }
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
-
     public String getContactName() {
         return contactName;
     }
-
     public void setContactName(String contactName) {
         this.contactName = contactName;
     }
-
     public String getContactEmail() {
         return contactEmail;
     }
-
     public void setContactEmail(String contactEmail) {
         this.contactEmail = contactEmail;
     }
-
     public Long getId() {
         return id;
     }
-
-    public Project getProjects() {
+    public List<Project> getProjects() {
         return projects;
     }
-
-    public void setProjects(Project projects) {
-        this.projects = projects;
+    public void addProject(Project aAdd){
+        this.projects.add(aAdd);
     }
 }
--- src/main/java/ourbusinessproject/Project.java
+++ src/main/java/ourbusinessproject/Project.java
@@ -1,49 +1,46 @@
 package ourbusinessproject;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-
 @Entity
 public class Project {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     @NotEmpty
     private String title;
     private String description;
-    @ManyToOne @NotNull
+    @ManyToOne(cascade=CascadeType.ALL)
     private Enterprise enterprise;
-
     public Project() {
     }
 
     public Project(Enterprise enterprise) {
         this.enterprise = enterprise;
+        this.enterprise.addProject(this);
     }
 
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
-
     public Long getId() {
         return id;
     }
 
     public void setEnterprise(Enterprise enterprise) {
         this.enterprise = enterprise;
+        this.enterprise.addProject(this);
+    }
+
+    public Enterprise getEnterprise() {
+        return enterprise;
     }
 }
