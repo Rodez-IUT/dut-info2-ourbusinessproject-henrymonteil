1511394ba5c71ee9584ff032037d0a25beebbf76
henry <henry.monteil@gmail.com>
2020-03-25 10:30:15 +0100

    fix #2.1.10 non regression OK

--- src/main/java/ourbusinessproject/Enterprise.java
+++ src/main/java/ourbusinessproject/Enterprise.java
@@ -1,12 +1,17 @@
 package ourbusinessproject;
+
 import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
+
 @Entity
 public class Enterprise {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
@@ -18,42 +23,53 @@ public class Enterprise {
     private String contactName;
     @NotEmpty @Email
     private String contactEmail;
-    @OneToMany(cascade=CascadeType.ALL)
+    @OneToMany(cascade=CascadeType.ALL) @NotNull
     private List<Project> projects;
 
     public Enterprise(){
         this.projects = new ArrayList<>();
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public String getContactName() {
         return contactName;
     }
+
     public void setContactName(String contactName) {
         this.contactName = contactName;
     }
+
     public String getContactEmail() {
         return contactEmail;
     }
+
     public void setContactEmail(String contactEmail) {
         this.contactEmail = contactEmail;
     }
+
     public Long getId() {
         return id;
     }
+
     public List<Project> getProjects() {
-        return projects;
+        return projects.isEmpty() ? null : projects;
     }
+
     public void addProject(Project aAdd){
         this.projects.add(aAdd);
     }
